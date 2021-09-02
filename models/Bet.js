19413318d7cb8d283e8f3f32deb743ea19f234f0
alexis lenoir <alexis59495@gmail.com>
2021-09-02 09:45:28 -0300

    Correct bet

--- models/Bet.js
+++ models/Bet.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const bet = new Schema({
-  user: { type: Schema.Types.ObjectId, ref: 'users' },
-  game: { type: String, ref: 'games' },
-  choice: Number
+  user: { type: Schema.Types.ObjectId, ref: 'users', required: true },
+  game: { type: String, ref: 'games', required: true },
+  choice: {type: Number, required: true}
 });
 
 mongoose.model('bets', bet);
