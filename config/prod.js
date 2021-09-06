2f148af7acb99a21e68752e6f65005c07482e904
alexis lenoir <alexis59495@gmail.com>
2021-09-06 12:48:39 -0300

    Add football api key in prod

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,6 @@ module.exports = {
   googleClientSecret : process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
-  oddsApi: process.env.ODDS_API
+  oddsApi: process.env.ODDS_API,
+  footballApiDataKey: process.env.FOOTBALL_API_KEY
 }
