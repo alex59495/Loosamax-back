c5ef6f10be8b9699b1675ac60ba08a9f22c6cc68
alexis lenoir <alexis59495@gmail.com>
2021-09-09 00:43:43 -0300

    Test endpoint Games

--- test/endpoints.test.js
+++ test/endpoints.test.js
@@ -1,13 +1,12 @@
-const mongoose = require('mongoose');
-const keys = require('../config/keys');
-const mongoDb = keys.mongoURI;
 const request = require('supertest');
 const dbHandler = require('./db-handler');
 
 const games = require('./data/games');
 const app = require('../app');
+const Game = require('../models/Game');
+const leaguesIdentifiers = require('../helpers/leaguesIdentifiers');
 
-describe('Should access current user', () => {
+describe('Games endpoints', () => {
 
   beforeAll(async () => {
     await dbHandler.connect()
@@ -22,9 +21,16 @@ describe('Should access current user', () => {
   });
 
   it('should access the games', async () => {
+    for(const game of games) {
+      const gameDb = new Game(game);
+      await gameDb.save();
+    };
+    const numGamesFoundFrance = games.filter(game => game.sport_key === leaguesIdentifiers['ligue_1'].name).length
     const res = await request(app)
       .get('/api/games/ligue_1')
-    console.log(res.body)
+      .expect('Content-Type', /json/)
+    
+    expect(res.body).toHaveLength(numGamesFoundFrance)
   })
 
 })
