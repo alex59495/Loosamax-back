7be9b2e2eca65c7ee84c3d3cff228abfe9b65e89
alexis lenoir <alexis59495@gmail.com>
2021-09-10 08:56:48 -0300

    Add comments

--- config/keys.js
+++ config/keys.js
@@ -2,6 +2,7 @@ switch(process.env.NODE_ENV) {
   case 'development':
     module.exports = require('./dev');
   // We use the same named variables in Github actions (testing) and Heroku
+  // Don't forget to declare the variables in the .github/worflows/node.js.yml
   default:
     module.exports = require('./prod');
 }
