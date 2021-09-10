db1c48dc220540860e661fbe22fdaa84e4de034b
alexis lenoir <alexis59495@gmail.com>
2021-09-10 08:38:21 -0300

    with keys

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,7 @@
-console.log(process.env.NODE_ENV)
 switch(process.env.NODE_ENV) {
-  case 'production':
-  // Use the same denomination in Github secret as we do in Heroku Var
-  case 'test':
-    module.exports = require('./prod');
-  default:
+  case 'development':
     module.exports = require('./dev');
+  // We use the same named variables in Github actions (testing) and Heroku
+  default:
+    module.exports = require('./prod');
 }
