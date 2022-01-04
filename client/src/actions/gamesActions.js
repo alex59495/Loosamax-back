3644e87a16f081681d2d7c0f97140821a79c31ae
alex59495 <alexis59495@gmail.com>
2022-01-04 09:44:44 -0300

    Filter match

--- client/src/actions/gamesActions.js
+++ client/src/actions/gamesActions.js
@@ -4,8 +4,11 @@ import { FETCH_GAMES } from './types';
 export const fetchGames = (league) => async (dispatch) => 
 {
   const res = await axios.get(`/api/games/${league}`)
+  const filterGames = res.data.filter(game => {
+    return game.commence_time > Date.now()
+  });
   const payload = {
-    [league]: res.data
+    [league]: filterGames
   }
   dispatch({ type: FETCH_GAMES, payload: payload })
 };
