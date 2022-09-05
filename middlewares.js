1e72e41fa2794c99568d56468d23101f6609f629
Alexis Lenoir <alexis.lenoir@sightcall.com>
2022-09-05 21:19:27 +0200

    Try other to add info in the middleware

--- middlewares.js
+++ middlewares.js
@@ -20,7 +20,8 @@ module.exports = (app) => {
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", keys.frontUrl);
     res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept");
     next();
 });
   
