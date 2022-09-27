376ab270607731ca24d6b262e47a2da481950ef3
Alexis Lenoir <alexis.lenoir@sightcall.com>
2022-09-27 22:42:25 +0200

    Try to cross-domain cookies

--- middlewares.js
+++ middlewares.js
@@ -13,29 +13,38 @@ module.exports = (app) => {
 
   webpush.setVapidDetails('mailto:maxence.lenoir1206@gmail.com', keys.publicVapid, keys.privateVapid);
 
-  app.set('trust proxy', 1);
-
-  app.use(cors({ credentials: true, origin: keys.frontUrl }))
-
-  // Resolve CORS issues
-  app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", keys.frontUrl);
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Set-Cookie, Cookie");
-    next();
-  });
-  
-  app.use(device.capture());
-  // Middlewares
-  app.use(express.json());
   app.use(
     cookieSession({
       // 30 days
       maxAge: 30 * 24 * 60 * 60 * 1000,
-      keys: [keys.cookieKey]
+      keys: [keys.cookieKey],
+      secure: process.env.NODE_ENV === 'development' ? false : true,
+      httpOnly: process.env.NODE_ENV === 'development' ? false : true,
+      sameSite: process.env.NODE_ENV === 'development' ? false : 'none',
     })
   );
+
+  app.enable('trust proxy');
+
+  // Resolve CORS issues
+  app.use(
+    cors({ 
+      credentials: true, 
+      origin: keys.frontUrl 
+    })
+  );
+
+  // app.use(function(req, res, next) {
+  //   res.header("Access-Control-Allow-Origin", keys.frontUrl);
+  //   res.header("Access-Control-Allow-Credentials", true);
+  //   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
+  //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Set-Cookie, Cookie");
+  //   next();
+  // });
+  
+  app.use(device.capture());
+  // Middlewares
+  app.use(express.json());
   app.use(flash());
   app.use(passport.initialize());
   app.use(passport.session());
