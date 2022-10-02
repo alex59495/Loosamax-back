68d52042a22412cf30d76dccfe3e219d91bfca41
Alexis Lenoir <alexis.lenoir@sightcall.com>
2022-10-02 19:11:53 +0200

    Fetch odd at 10 pm

--- cronJobs.js
+++ cronJobs.js
@@ -2,26 +2,24 @@ const cronJob = require('node-cron');
 
 const FetchOdd = require('./jobs/jobFetchOdd.js');
 const UpdateResult = require('./jobs/jobUpdateResult.js');
-const AutomaticChooseGame = require('./jobs/jobAutomaticChooseGame')
+const AutomaticChooseGame = require('./jobs/jobAutomaticChooseGame');
 
 module.exports = () => {
-  // Every hour
-  const scheduledHourlyJobs = cronJob.schedule("0 * * * *", () => {
-    FetchOdd.call()
+  const scheduledDailyyJobs = cronJob.schedule('0 10 * * *', () => {
+    FetchOdd.call();
+  });
+
+  const scheduled10MinJobs = cronJob.schedule('*/10 * * * *', () => {
+    UpdateResult.call();
     // Add your custom logic here
   });
 
-  const scheduled10MinJobs = cronJob.schedule("*/10 * * * *", () => {
-    UpdateResult.call()
+  const scheduledFridayJobs = cronJob.schedule('* 19 * * Friday', () => {
+    AutomaticChooseGame.call();
     // Add your custom logic here
   });
 
-  const scheduledFridayJobs = cronJob.schedule("* 19 * * Friday", () => {
-    AutomaticChooseGame.call()
-    // Add your custom logic here
-  });
-
-  scheduledHourlyJobs.start();
+  scheduledDailyyJobs.start();
   scheduled10MinJobs.start();
   scheduledFridayJobs.start();
-}
+};
