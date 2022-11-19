d50eade53e74837f8b014b93ab89c1242b11af6f
Alexis Lenoir <alexis.lenoir@sightcall.com>
2022-11-19 15:38:00 +0100

    Use dotenv

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const app = require('./app');
 const express = require('express');
 const initScheduledJobs = require('./cronJobs');
+require('dotenv').config();
 
 process.env.TZ = 'Europe/Paris';
 
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets like main.css or main.js
   app.use(express.static('client/build'));
   // Express will serve up index.html file if it doesn't recognize the route
@@ -14,7 +15,7 @@ if(process.env.NODE_ENV === 'production') {
   });
 }
 
-initScheduledJobs()
+initScheduledJobs();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
