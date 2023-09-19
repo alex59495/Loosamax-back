b6f3eaefb10646b6a2b6e9223d1a825eeb511e38
alexis.lenoir <alexis.lenoir@sightcall.com>
2023-09-19 21:40:42 +0200

    Remove config/dev.js from the gitignore

---
+++ config/dev.js
@@ -0,0 +1,12 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  oddsApi: process.env.ODDS_API,
+  footballApiDataKey: process.env.FOOTBALL_API_KEY,
+  redisUrl: process.env.REDIS_URL,
+  googleIDMax: process.env.GOOGLE_ID_MAX,
+  googleIDAlex: process.env.GOOGLE_ID_ALEX,
+  frontUrl: process.env.FRONT_URL,
+};
